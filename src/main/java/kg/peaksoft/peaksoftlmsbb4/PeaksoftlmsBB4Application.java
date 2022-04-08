986bc4b72360bd2da8addf9b7d2514d4e87afdbf
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-04-08 15:31:42 +0600

    create empty project

---
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/PeaksoftlmsBB4Application.java
@@ -0,0 +1,14 @@
+package kg.peaksoft.peaksoftlmsbb4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeaksoftlmsBB4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeaksoftlmsBB4Application.class, args);
+        System.out.println("Welcome colleagues, project name is Peaksoftlms-B");
+    }
+
+}
---
+++ src/test/java/kg/peaksoft/peaksoftlmsbb4/PeaksoftlmsBB4ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.peaksoft.peaksoftlmsbb4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeaksoftlmsBB4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
