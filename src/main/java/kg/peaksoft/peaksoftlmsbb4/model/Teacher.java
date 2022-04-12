2e1b8a4a9a57575ba10e4146df56220ab69af783
Aisuluu <intellijei.123@gmail.com>
2022-04-12 13:49:13 +0600

    add User

---
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/model/Teacher.java
@@ -0,0 +1,37 @@
+package kg.peaksoft.peaksoftlmsbb4.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+import static javax.persistence.CascadeType.ALL;
+
+@Entity
+@Table(name = "teachers")
+@Data
+public class Teacher {
+
+    @Id
+    @SequenceGenerator(
+            name = "teacher_sequence",
+            sequenceName = "teacher_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "teacher_sequence"
+    )
+    private Long id;
+
+    private String teacherName;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    private String specialization;
+
+    @OneToOne(cascade = ALL, fetch = FetchType.EAGER)
+    private User user;
+
+}
