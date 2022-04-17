5fca3e830759fd68bb54b7777bc5f5770510e6e3
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-04-17 17:19:23 +0600

    AWS CI/CD config

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/PeaksoftlmsBB4Application.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/PeaksoftlmsBB4Application.java
@@ -3,15 +3,24 @@ package kg.peaksoft.peaksoftlmsbb4;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 @AllArgsConstructor
 public class PeaksoftlmsBB4Application {
+
+    @GetMapping("/")
+    public String greetingPage(){
+        return "<h1>Welcome to Peaksoftlms-B application!!!<h1/>";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(PeaksoftlmsBB4Application.class, args);
         System.out.println("Welcome colleagues, project name is Peaksoftlms-B");
     }
-    }
+}
 
 
 
--- src/main/resources/db/migration/V1__createTable.sql
+++ src/main/resources/db/migration/V1__createTable.sql
@@ -1,66 +1,66 @@
-create table student(
-    id bigint not null
-        constraint student_pkey
-            primary key,
-    last_name    varchar(255),
-    phone_number varchar(255),
-    student_name varchar(255),
-    study_format varchar(255),
-    user_id      bigint
-        constraint fkk0thg920a3xk3v59yjbsatw1l
-            references users
-);
-
-create table teachers(
-    id bigint not null
-        constraint teachers_pkey
-            primary key,
-    last_name      varchar(255),
-    phone_number   varchar(255),
-    specialization varchar(255),
-    teacher_name   varchar(255),
-    user_id        bigint
-        constraint fkb8dct7w2j1vl1r2bpstw5isc0
-            references users
-);
-
-create table users(
-    id                         bigint  not null
-        constraint users_pkey
-            primary key,
-    email                      varchar(255),
-    is_account_non_expired     boolean not null,
-    is_account_non_locked      boolean not null,
-    is_credentials_non_expired boolean not null,
-    is_enabled                 boolean not null,
-    password                   varchar(255),
-    role                       varchar(255)
-);
-
-
-create table groups(
-    id            bigint not null
-        constraint groups_pkey
-            primary key,
-    date_of_start date,
-    description   varchar(255),
-    group_name    varchar(255),
-    imagine       varchar(255)
-);
-
-create table admin(
-    id         bigint not null
-        constraint admin_pkey
-            primary key,
-    first_name varchar(255),
-    last_name  varchar(255),
-    user_id    bigint
-        constraint fkq7pdkck9je126wpd9ijw3uwml
-            references users
-);
-
-
-
-
-
-
+-- create table student(
+--     id bigint not null
+--         constraint student_pkey
+--             primary key,
+--     last_name    varchar(255),
+--     phone_number varchar(255),
+--     student_name varchar(255),
+--     study_format varchar(255),
+--     user_id      bigint
+--         constraint fkk0thg920a3xk3v59yjbsatw1l
+--             references users
+-- );
+--
+-- create table teachers(
+--     id bigint not null
+--         constraint teachers_pkey
+--             primary key,
+--     last_name      varchar(255),
+--     phone_number   varchar(255),
+--     specialization varchar(255),
+--     teacher_name   varchar(255),
+--     user_id        bigint
+--         constraint fkb8dct7w2j1vl1r2bpstw5isc0
+--             references users
+-- );
+--
+-- create table users(
+--     id                         bigint  not null
+--         constraint users_pkey
+--             primary key,
+--     email                      varchar(255),
+--     is_account_non_expired     boolean not null,
+--     is_account_non_locked      boolean not null,
+--     is_credentials_non_expired boolean not null,
+--     is_enabled                 boolean not null,
+--     password                   varchar(255),
+--     role                       varchar(255)
+-- );
+--
+--
+-- create table groups(
+--     id            bigint not null
+--         constraint groups_pkey
+--             primary key,
+--     date_of_start date,
+--     description   varchar(255),
+--     group_name    varchar(255),
+--     imagine       varchar(255)
+-- );
+--
+-- create table admin(
+--     id         bigint not null
+--         constraint admin_pkey
+--             primary key,
+--     first_name varchar(255),
+--     last_name  varchar(255),
+--     user_id    bigint
+--         constraint fkq7pdkck9je126wpd9ijw3uwml
+--             references users
+-- );
+--
+--
+--
+--
+--
+--
