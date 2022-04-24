fcd0842a3847b7c586790adf6f4c78a9ce224735
Nurisaaa <nurisa.abdykalykova>
2022-04-24 23:45:31 +0600

    edit , additions

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/db/repository/AdminRepository.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/db/repository/AdminRepository.java
@@ -1,6 +1,5 @@
 package kg.peaksoft.peaksoftlmsbb4.db.repository;
 
-import kg.peaksoft.peaksoftlmsbb4.db.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
