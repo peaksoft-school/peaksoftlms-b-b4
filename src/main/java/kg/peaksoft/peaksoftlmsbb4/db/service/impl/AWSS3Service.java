a8f4e6204c070c0af4e11c18806cac1c538a3cb4
rahim003 <jhhhk>
2022-05-13 18:01:54 +0300

    refactor/s3

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/impl/AWSS3Service.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/impl/AWSS3Service.java
@@ -1,8 +1,8 @@
 package kg.peaksoft.peaksoftlmsbb4.db.service.impl;
 
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.util.IOUtils;
 import kg.peaksoft.peaksoftlmsbb4.db.service.FileService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -34,23 +34,37 @@ public class AWSS3Service implements FileService {
         metaData.setContentType(file.getContentType());
 
         try {
-            awsS3Client.putObject("peaksoft-lms-b4", key, file.getInputStream(), metaData);
+            awsS3Client.putObject("peaksoft-lms", key, file.getInputStream(), metaData);
             log.info("upload the file");
         } catch (IOException e) {
             log.error("an exception occured while uploading the file");
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "An exception occured while uploading the file");
         }
 
-        awsS3Client.setObjectAcl("peaksoft-lms-b4", key, CannedAccessControlList.PublicRead);
+        awsS3Client.setObjectAcl("peaksoft-lms", key, CannedAccessControlList.PublicRead);
 
-        return awsS3Client.getResourceUrl("peaksoft-lms-b4", key);
+        return awsS3Client.getResourceUrl("peaksoft-lms", key);
     }
 
     @Override
     public String deleteFile(String file) {
         String[] fileName = file.split("/");
-        awsS3Client.deleteObject("peaksoft-lms-b4", fileName[fileName.length-1]);
+        awsS3Client.deleteObject("peaksoft-lms", fileName[fileName.length-1]);
         log.info("File deleted");
         return "Deleted File: " + file;
     }
+
+    @Override
+    public byte[] downloadFile(String file) {
+        S3Object object = awsS3Client.getObject("peaksoft-lms", file);
+        S3ObjectInputStream objectContent = object.getObjectContent();
+        try {
+            return IOUtils.toByteArray(objectContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
