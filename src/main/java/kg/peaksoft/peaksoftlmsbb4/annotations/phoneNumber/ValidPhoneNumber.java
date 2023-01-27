d409728100488da602546110db9eaeb76900156d
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-27 17:46:31 +0600

    refactor ValidPhoneNumber

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/annotations/phoneNumber/ValidPhoneNumber.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/annotations/phoneNumber/ValidPhoneNumber.java
@@ -1,20 +1,24 @@
 package kg.peaksoft.peaksoftlmsbb4.annotations.phoneNumber;
 
-
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.*;
 
-
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER,
+@Target({ElementType.METHOD,
+        ElementType.FIELD,
+        ElementType.ANNOTATION_TYPE,
+        ElementType.CONSTRUCTOR,
+        ElementType.PARAMETER,
         ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Constraint(validatedBy = PhoneNumberValidator.class)
 public @interface ValidPhoneNumber {
+
     String message() default "Invalid phone number";
 
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
+
 }
