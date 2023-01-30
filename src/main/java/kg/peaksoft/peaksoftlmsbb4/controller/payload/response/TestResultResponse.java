54241070b5440986388835c7d03456036acbb841
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-30 17:58:34 +0600

    refactor TestResultResponse

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/payload/response/TestResultResponse.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/payload/response/TestResultResponse.java
@@ -4,10 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+
 @Getter
 @Setter
 public class TestResultResponse {
+
     private boolean testIsEnabled;
     private String name;
     private List<ResultResponse> resultResponses;
+
 }
