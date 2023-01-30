45c72d11b11a0a44afb6b1bc27603d9e7413e7cd
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-30 17:57:51 +0600

    refactor TeacherResponse

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/payload/response/TeacherResponse.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/payload/response/TeacherResponse.java
@@ -8,17 +8,11 @@ import lombok.Setter;
 public class TeacherResponse {
 
     private Long id;
-
     private String teacherName;
-
     private String lastName;
-
     private String phoneNumber;
-
     private String specialization;
-
     private String email;
-
     private String fullName;
 
 }
