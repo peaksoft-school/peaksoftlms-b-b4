0e2e2696ef2580a49f29c2f4e3bb525fdd56375f
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-30 17:49:43 +0600

    refactor TestRequest

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/payload/request/TestRequest.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/payload/request/TestRequest.java
@@ -8,8 +8,10 @@ import java.util.List;
 @Getter
 @Setter
 public class TestRequest {
+
     private String testName;
     private Boolean disable = false;
     private Long lessonsId;
     private List<QuestionRequest> questionRequestList;
+
 }
