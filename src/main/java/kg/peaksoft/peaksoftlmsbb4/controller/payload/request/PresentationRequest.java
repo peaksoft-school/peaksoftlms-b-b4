ae7cf73b7c27fd83f1eb58488472bea78f2276b3
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-30 17:46:12 +0600

    refactor PresentationRequest

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/payload/request/PresentationRequest.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/payload/request/PresentationRequest.java
@@ -8,12 +8,16 @@ import javax.validation.constraints.NotBlank;
 @Getter
 @Setter
 public class PresentationRequest {
+
     @NotBlank
     private String name;
+
     @NotBlank
     private String description;
+
     @NotBlank
     private String file;
+
     private Long lessonId;
 
 }
