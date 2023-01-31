f211061de487edb41ebc364b5a3971d755a5a5b4
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-31 16:28:57 +0600

    refactor LessonsApi

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/LessonsApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/LessonsApi.java
@@ -10,17 +10,18 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.LessonRequest;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.LessonResponse;
 import kg.peaksoft.peaksoftlmsbb4.db.service.LessonService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("api/lessons")
-@Tag(name = "Lesson", description = "The Lesson API")
+@PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
 @CrossOrigin(origins = "*", maxAge = 3600)
+@Tag(name = "Lesson API", description = "The Lesson endpoints")
 public class LessonsApi {
 
     private final LessonService lessonService;
@@ -28,7 +29,6 @@ public class LessonsApi {
     @PostMapping
     @Operation(summary = "Add new lesson to course",
             description = "This endpoint save new lesson to course by ID. Only users with role teacher can add new lessons")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
     public LessonResponse saveLesson(@RequestBody LessonRequest lessonRequest) {
         return lessonService.saveLessons(lessonRequest);
     }
@@ -36,7 +36,7 @@ public class LessonsApi {
     @GetMapping("/{id}")
     @Operation(summary = "Gets a single lessons by identifier",
             description = "For valid response try integer IDs with value >= 1 and...")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR','STUDENT')")
+    @PreAuthorize("hasAnyAuthority('STUDENT')")
     public LessonResponse findById(@PathVariable Long id) {
         return lessonService.findById(id);
     }
@@ -50,7 +50,7 @@ public class LessonsApi {
                     content = {
                             @Content(mediaType = "application/json",
                                     array = @ArraySchema(schema = @Schema(implementation = LessonsApi.class)))})})
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR','STUDENT')")
+    @PreAuthorize("hasAnyAuthority('STUDENT')")
     public List<LessonResponse> findAll(@PathVariable Long id) {
         return lessonService.findAll(id);
     }
@@ -58,14 +58,14 @@ public class LessonsApi {
     @PutMapping("/{id}")
     @Operation(summary = "Update the lessons",
             description = "Updates the details of an endpoint with ID. Only users with role teacher can update the lesson")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
+
     public LessonResponse update(@PathVariable Long id, @RequestBody LessonRequest lessonRequest) {
         return lessonService.update(id, lessonRequest);
 
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
+
     @Operation(summary = "Delete the lesson",
             description = "Delete lesson with ID. Only users with role teacher can delete lesson")
     public LessonResponse delete(@PathVariable Long id) {
