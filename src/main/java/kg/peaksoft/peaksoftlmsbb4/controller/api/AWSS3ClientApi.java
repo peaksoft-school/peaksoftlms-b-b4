f8dc6040932ffc1e0c4cea1ed8e11df09be92635
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-31 13:50:02 +0600

    refactor AWSS3ClientApi

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/AWSS3ClientApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/AWSS3ClientApi.java
@@ -1,8 +1,9 @@
 package kg.peaksoft.peaksoftlmsbb4.controller.api;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.peaksoft.peaksoftlmsbb4.db.service.impl.AWSS3Service;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,15 +18,17 @@ import java.util.Map;
 import static java.net.HttpURLConnection.HTTP_OK;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("api/files")
 @CrossOrigin(origins = "*", maxAge = 3600)
+@PreAuthorize("hasAnyAuthority('ADMIN','INSTRUCTOR')")
+@Tag(name = "File API", description = "Aws S3 file endpoints")
 public class AWSS3ClientApi {
+
     private AWSS3Service awsS3Service;
 
     @Operation(summary = "Upload file", description = "This endpoint for upload file to s3")
-    @PostMapping("/upload")
-    @PreAuthorize("hasAnyAuthority('ADMIN','INSTRUCTOR')")
+    @PostMapping("upload")
     public ResponseEntity<Map<String, String>> uploadFile(@RequestParam("file") MultipartFile file) {
         String publicURL = awsS3Service.uploadFile(file);
         Map<String, String> response = new HashMap<>();
@@ -33,16 +36,14 @@ public class AWSS3ClientApi {
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    @Operation(summary = "Delete the file ",
-            description = "Delete file in s3")
-    @PreAuthorize("hasAnyAuthority('ADMIN','INSTRUCTOR')")
-    @DeleteMapping("/delete")
+    @Operation(summary = "Delete the file ", description = "Delete file in s3")
+    @DeleteMapping("delete")
     public String deleteFile(@RequestParam String file) {
         return awsS3Service.deleteFile(file);
     }
 
     @Operation(summary = "Download file", description = "Download file from aws s3 repository by file name")
-    @GetMapping("/download/{filename}")
+    @GetMapping("download/{filename}")
     public ResponseEntity<byte[]> download(@PathVariable("filename") String filename) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-type", MediaType.ALL_VALUE);
@@ -50,4 +51,5 @@ public class AWSS3ClientApi {
         byte[] bytes = awsS3Service.downloadFile(filename);
         return ResponseEntity.status(HTTP_OK).headers(headers).body(bytes);
     }
+
 }
