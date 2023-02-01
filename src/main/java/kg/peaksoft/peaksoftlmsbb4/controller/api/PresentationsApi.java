2e7f995b649040adbbf842484faf76c6c73fb612
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-01 21:45:45 +0600

    refactor PresentationsApi

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/PresentationsApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/PresentationsApi.java
@@ -5,58 +5,54 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.PresentationRequest;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.PresentationResponse;
 import kg.peaksoft.peaksoftlmsbb4.db.service.PresentationService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("api/presentations")
-@Tag(name = "Presentations", description = "The Presentations API")
+@PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
 @CrossOrigin(origins = "*", maxAge = 3600)
+@Tag(name = "Presentations API", description = "The Presentations endpoints")
 public class PresentationsApi {
 
     private final PresentationService presentationService;
 
+    @Operation(summary = "Add new presentation", description = "This endpoint save new presentations. Only users with role teacher can add new presentation to lesson")
     @PostMapping
-    @Operation(summary = "Add new presentation",
-            description = "This endpoint save new presentations. Only users with role teacher can add new presentation to lesson")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
     public PresentationResponse savePresentations(@RequestBody PresentationRequest presentationRequest) {
         return presentationService.savePresentation(presentationRequest);
     }
 
-    @GetMapping("/{id}")
-    @Operation(summary = "Gets a single presentation by identifier",
-            description = "For valid response try integer IDs with value >= 1 and...")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR','STUDENT')")
+
+    @Operation(summary = "Gets a single presentation by identifier", description = "For valid response try integer IDs with value >= 1 and...")
+    @PreAuthorize("hasAnyAuthority('STUDENT')")
+    @GetMapping("{id}")
     public PresentationResponse findById(@PathVariable Long id) {
         return presentationService.findById(id);
-
     }
 
-    @PutMapping("/{id}")
 
-    @Operation(summary = "Update the presentations",
-            description = "Updates the details of an endpoint with ID")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
-    public PresentationResponse update(@PathVariable Long id,
-                                       @RequestBody PresentationRequest presentationRequest) {
+
+    @Operation(summary = "Update the presentations", description = "Updates the details of an endpoint with ID")
+    @PutMapping("{id}")
+    public PresentationResponse update(@PathVariable Long id, @RequestBody PresentationRequest presentationRequest) {
         return presentationService.update(id, presentationRequest);
     }
 
-    @DeleteMapping("/{id}")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
-    @Operation(summary = "Delete the presentation",
-            description = "Delete links with ID. Only users with role teacher can delete links")
+
+
+    @Operation(summary = "Delete the presentation", description = "Delete links with ID. Only users with role teacher can delete links")
+    @DeleteMapping("{id}")
     public PresentationResponse delete(@PathVariable Long id) {
         return presentationService.delete(id);
     }
 
+
+    @Operation(summary = "Gets a single presentation by lesson identifier", description = "For valid response try integer IDs with value >= 1 and...")
+    @PreAuthorize("hasAnyAuthority('STUDENT')")
     @GetMapping("LessonPresentation/{id}")
-    @Operation(summary = "Gets a single presentation by lesson identifier",
-            description = "For valid response try integer IDs with value >= 1 and...")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR',STUDENT)")
     public PresentationResponse getPresentationByLessonId(@PathVariable Long id) {
         return presentationService.findPresentationByLessonId(id);
     }
