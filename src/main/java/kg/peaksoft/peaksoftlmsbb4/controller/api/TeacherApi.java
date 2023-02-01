679e49c7da57cc2c5fc3e209301f6b3d1474a94b
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-01 22:10:03 +0600

    refactor TeacherApi

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/TeacherApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/TeacherApi.java
@@ -7,19 +7,19 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.CourseResponse;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.AssignGroupRequest;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.AssignStudentRequest;
+import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.TeacherRequest;
+import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.CourseResponse;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.StudentResponse;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.TeacherPaginationResponse;
-import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.TeacherRequest;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.TeacherResponse;
 import kg.peaksoft.peaksoftlmsbb4.db.model.User;
 import kg.peaksoft.peaksoftlmsbb4.db.service.CourseService;
 import kg.peaksoft.peaksoftlmsbb4.db.service.GroupService;
 import kg.peaksoft.peaksoftlmsbb4.db.service.StudentService;
 import kg.peaksoft.peaksoftlmsbb4.db.service.TeacherService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -29,10 +29,11 @@ import java.util.Deque;
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
-@RequestMapping("/api/teachers")
+@RequiredArgsConstructor
+@RequestMapping("api/teachers")
+@PreAuthorize("hasAnyAuthority('ADMIN')")
 @CrossOrigin(origins = "*", maxAge = 3600)
-@Tag(name = "Teacher", description = "The Teacher API")
+@Tag(name = "Teacher API", description = "The Teacher endpoints")
 public class TeacherApi {
 
     private final TeacherService teacherService;
@@ -49,7 +50,6 @@ public class TeacherApi {
                     content = {
                             @Content(mediaType = "application/json",
                                     array = @ArraySchema(schema = @Schema(implementation = TeacherApi.class)))})})
-    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public TeacherPaginationResponse findAll(@RequestParam int page,
                                              @RequestParam int size) {
         return teacherService.findAllTeacher(--page, size);
@@ -57,7 +57,6 @@ public class TeacherApi {
 
     @Operation(summary = "Add new teacher",
             description = "This endpoint save new teacher")
-    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @PostMapping
     public TeacherResponse saveTeacher(@Valid @RequestBody TeacherRequest teacherRequest) {
         return teacherService.saveTeacher(teacherRequest);
@@ -66,7 +65,6 @@ public class TeacherApi {
     @PatchMapping("/{id}")
     @Operation(summary = "Update the teacher",
             description = "Updates the details of an endpoint with ID")
-    @PreAuthorize("hasAuthority('ADMIN')")
     public TeacherResponse updateTeacher(@PathVariable("id") Long id, @RequestBody TeacherRequest teacherRequest) {
         return teacherService.updateTeacher(id, teacherRequest);
     }
@@ -74,7 +72,6 @@ public class TeacherApi {
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete the teacher",
             description = "Delete the teacher with ID")
-    @PreAuthorize("hasAuthority('ADMIN')")
     public TeacherResponse deleteTeacher(@PathVariable("id") Long id) {
         return teacherService.deleteTeacher(id);
     }
@@ -88,7 +85,7 @@ public class TeacherApi {
         return teacherService.teacherCourses(user.getEmail());
     }
 
-    @PreAuthorize("hasAnyAuthority('ADMIN')")
+
     @Operation(summary = "Gets a single teacher by identifier",
             description = "For valid response try integer IDs with value >= 1 and...")
     @GetMapping("/{id}")
@@ -107,14 +104,13 @@ public class TeacherApi {
     @Operation(summary = "Assign group to course",
             description = "This endpoint for adding a group to course")
     @PostMapping("/assignGroup")
-    @PreAuthorize("hasAnyAuthority('ADMIN','INSTRUCTOR')")
+    @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
     public String assignGroupToCourse(@RequestBody AssignGroupRequest assignGroupRequest) {
         return groupService.assignGroupToCourse(assignGroupRequest);
     }
 
     @Operation(summary = "Course's teachers",
             description = "Get all teachers ")
-    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @GetMapping("teachersByCourseId/{id}")
     public List<TeacherResponse> getAllTeacherByCourseId(@PathVariable Long id) {
         return courseService.getAllTeacherByCourseId(id);
@@ -122,7 +118,6 @@ public class TeacherApi {
 
     @Operation(summary = "find all teachers",
             description = "This method is a method of adding teachers to a course so you can give how many teachers per course ")
-    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @GetMapping("Unassigned/{id}")
     public Deque<TeacherResponse> teacherResponses(@PathVariable Long id) {
         return teacherService.teacherResponsesForAssign(id);
