193628759a75091e85ace804381ac69aaa6d956f
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-01 22:26:29 +0600

    refactor VariantApi

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/VariantApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/VariantApi.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.VariantRequest;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.VariantResponse;
 import kg.peaksoft.peaksoftlmsbb4.db.service.VariantService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,31 +13,29 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
-@RequestMapping("/api/variants")
-@CrossOrigin(origins = "*", maxAge = 3600)
-@Tag(name = "Variant", description = "The Variant API")
+@RequiredArgsConstructor
+@RequestMapping("api/variants")
 @PreAuthorize("hasAnyAuthority('INTRUCTOR,STUDENT')")
+@CrossOrigin(origins = "*", maxAge = 3600)
+@Tag(name = "Variant API", description = "The Variant endpoints")
 public class VariantApi {
+
     private final VariantService variantService;
 
-    @Operation(summary = "Gets a single variant by identifier",
-            description = "For valid response try integer IDs with value >= 1 and...")
-    @GetMapping("/{id}")
+    @Operation(summary = "Gets a single variant by identifier", description = "For valid response try integer IDs with value >= 1 and...")
+    @GetMapping("{id}")
     public VariantResponse findById(@Valid @PathVariable Long id) {
         return variantService.findById(id);
     }
 
-    @Operation(summary = "Update the variant",
-            description = "Updates the details of an endpoint with ID")
-    @PutMapping("/{id}")
-    public VariantResponse updateVariant(@Valid @PathVariable Long id, @RequestBody VariantRequest variantRequest) {
-        return variantService.update(id, variantRequest);
+    @Operation(summary = "Update the variant", description = "Updates the details of an endpoint with ID")
+    @PutMapping("{id}")
+    public VariantResponse updateVariant(@Valid @PathVariable Long id, @RequestBody VariantRequest request) {
+        return variantService.update(id, request);
     }
 
-    @Operation(summary = "Delete variant",
-            description = "Delete the variant by ID")
-    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete variant", description = "Delete the variant by ID")
+    @DeleteMapping("{id}")
     public String deleteById(@Valid @PathVariable Long id) {
         variantService.delete(id);
         return String.format("successful variant delete by id=%s", id);
@@ -47,4 +45,5 @@ public class VariantApi {
     public List<VariantResponse> findAll() {
         return variantService.findAll();
     }
+
 }
