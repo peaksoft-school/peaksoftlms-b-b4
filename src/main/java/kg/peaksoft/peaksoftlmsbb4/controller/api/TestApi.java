9bb01d2e3322082d66fe578bdc41b08f2aeffef8
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-01 22:22:08 +0600

    refactor TestApi

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/TestApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/TestApi.java
@@ -6,65 +6,56 @@ import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.SwitcherRequest;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.TestRequest;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.TestResponse;
 import kg.peaksoft.peaksoftlmsbb4.db.service.TestService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.security.PermitAll;
+
 
 @RestController
-@AllArgsConstructor
-@RequestMapping("/api/tests")
+@RequiredArgsConstructor
+@RequestMapping("api/tests")
+@PreAuthorize("hasAuthority('INSTRUCTOR')")
 @CrossOrigin(origins = "*", maxAge = 3600)
-@Tag(name = "Test", description = "The Test API")
+@Tag(name = "Test API", description = "The Test endpoints")
 public class TestApi {
+
     private final TestService testService;
 
-    @Operation(summary = "Add new test",
-            description = "This endpoint create new test. Only users with role teacher can add new task to lesson")
-    @PreAuthorize("hasAuthority('INSTRUCTOR')")
-    @PermitAll
+    @Operation(summary = "Add new test", description = "This endpoint create new test. Only users with role teacher can add new task to lesson")
     @PostMapping()
     public TestResponse saveTest(@RequestBody TestRequest testRequest) {
         return testService.saveTest(testRequest);
     }
 
-    @Operation(summary = "Gets a single test by identifier",
-            description = "For valid response try integer IDs with value >= 1 and...")
-    @PreAuthorize("hasAnyAuthority('STUDENT','INSTRUCTOR')")
+    @Operation(summary = "Gets a single test by identifier", description = "For valid response try integer IDs with value >= 1 and...")
+    @PreAuthorize("hasAnyAuthority('STUDENT')")
     @GetMapping("/{id}")
     public TestResponse findById(@PathVariable Long id) {
         return testService.findById(id);
     }
 
-    @Operation(summary = "Find test by lesson ID",
-            description = "Gets a single tasks by lesson identifier")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR','STUDENT')")
+    @Operation(summary = "Find test by lesson ID", description = "Gets a single tasks by lesson identifier")
+    @PreAuthorize("hasAnyAuthority('STUDENT')")
     @GetMapping("lesson/{id}")
     public TestResponse findByLessonId(@PathVariable Long id) {
         return testService.findByLessonId(id);
     }
 
-    @Operation(summary = "Update test",
-            description = "Updates the details of an endpoint with ID")
+    @Operation(summary = "Update test", description = "Updates the details of an endpoint with ID")
     @PutMapping("{id}")
-    @PreAuthorize("hasAuthority('INSTRUCTOR')")
     public TestResponse updateTest(@PathVariable Long id, @RequestBody TestRequest testRequest) {
         return testService.update(id, testRequest);
     }
 
-    @Operation(summary = "Delete test",
-            description = "Delete the test with id")
-    @PreAuthorize("hasAuthority('INSTRUCTOR')")
+    @Operation(summary = "Delete test", description = "Delete the test with id")
     @DeleteMapping("/{id}")
     public String deleteTest(@PathVariable Long id) {
         testService.delete(id);
         return String.format("successfully delete this id=%s", id);
     }
 
-    @Operation(summary = "Switcher",
-            description = "With this endpoint you can disable or enable test")
-    @PreAuthorize("hasAuthority('INSTRUCTOR')")
+    @Operation(summary = "Switcher", description = "With this endpoint you can disable or enable test")
     @PutMapping("switcher/{id}")
     public boolean switcher(@PathVariable Long id, @RequestBody SwitcherRequest switcherRequest) {
         return testService.switcher(id, switcherRequest);
