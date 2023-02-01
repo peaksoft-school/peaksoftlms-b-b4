de4133e40122cdf411e1cc007b024336f905120a
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-01 21:49:45 +0600

    refactor QuestionApi

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/QuestionApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/QuestionApi.java
@@ -5,15 +5,16 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.QuestionRequest;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.QuestionResponse;
 import kg.peaksoft.peaksoftlmsbb4.db.service.QuestionService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
-@AllArgsConstructor
-@RequestMapping("/api/questions")
+@RequiredArgsConstructor
+@RequestMapping("api/questions")
+@PreAuthorize("hasAuthority('INSTRUCTOR')")
 @CrossOrigin(origins = "*", maxAge = 3600)
 @Tag(name = "Question", description = "The Question API")
 public class QuestionApi {
@@ -22,7 +23,6 @@ public class QuestionApi {
     @Operation(summary = "Gets a single question by identifier",
             description = "For valid response try integer IDs with value >= 1 and...")
     @GetMapping("/{id}")
-    @PreAuthorize("hasAuthority('INSTRUCTOR')")
     public QuestionResponse findById(@Valid @PathVariable Long id) {
         return questionService.findById(id);
     }
@@ -30,7 +30,6 @@ public class QuestionApi {
     @Operation(summary = "Update the question",
             description = "Updates the details of an endpoint with ID")
     @PutMapping("/{id}")
-    @PreAuthorize("hasAuthority('INSTRUCTOR')")
     public QuestionResponse updateQuestion(
             @Valid @PathVariable Long id, @RequestBody QuestionRequest questionRequest) {
         return questionService.update(id, questionRequest);
@@ -39,7 +38,6 @@ public class QuestionApi {
     @Operation(summary = "Delete question",
             description = "Delete the question with ID")
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasAuthority('INSTRUCTOR')")
     public String deleteById(@Valid @PathVariable Long id) {
         questionService.delete(id);
         return String.format("successfully delete this id=%s", id);
