cfaac9b13b9bc5a2fd9f76297a305d376d0956aa
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-01 21:57:41 +0600

    refactor StudentApi

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/StudentApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/StudentApi.java
@@ -7,18 +7,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.CourseResponse;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.AnswerRequest;
-import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.AnswerResponse;
-import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.StudentPaginationResponse;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.StudentRequest;
+import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.AnswerResponse;
+import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.CourseResponse;
+import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.StudentPaginationResponse;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.StudentResponse;
 import kg.peaksoft.peaksoftlmsbb4.db.enums.StudyFormat;
 import kg.peaksoft.peaksoftlmsbb4.db.model.User;
 import kg.peaksoft.peaksoftlmsbb4.db.service.ResultService;
 import kg.peaksoft.peaksoftlmsbb4.db.service.StudentService;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -29,11 +28,11 @@ import java.util.Deque;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/students")
-@AllArgsConstructor
+@RequiredArgsConstructor
+@RequestMapping("api/students")
+@PreAuthorize("hasAuthority('ADMIN')")
 @CrossOrigin(origins = "*", maxAge = 3600)
-@Tag(name = "Student", description = "The Student API")
-@Slf4j
+@Tag(name = "Student API", description = "The Student endpoints")
 public class StudentApi {
 
     private final StudentService studentService;
@@ -48,7 +47,7 @@ public class StudentApi {
                     content = {
                             @Content(mediaType = "application/json",
                                     array = @ArraySchema(schema = @Schema(implementation = StudentApi.class)))})})
-    @PreAuthorize("hasAnyAuthority('ADMIN','INSTRUCTOR')")
+    @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
     public StudentPaginationResponse getAll(@RequestParam int page,
                                             @RequestParam int size,
                                             @RequestParam StudyFormat studyFormat) {
@@ -59,7 +58,6 @@ public class StudentApi {
     @PostMapping
     @Operation(summary = "Add new student",
             description = "This endpoint save new student. Only users with role admin can add new students")
-    @PreAuthorize("hasAuthority('ADMIN')")
     public StudentResponse saveStudent(@RequestBody @Valid StudentRequest studentRequest) {
         return studentService.saveStudent(studentRequest);
     }
@@ -67,7 +65,6 @@ public class StudentApi {
     @PutMapping("/{id}")
     @Operation(summary = "Update the students",
             description = "Updates the details of an endpoint with ID.  Only users with role admin can update students")
-    @PreAuthorize("hasAuthority('ADMIN')")
     public StudentResponse updateStudent(@PathVariable("id") Long id,
                                          @RequestBody @Valid StudentRequest studentRequest) {
         return studentService.updateStudent(id, studentRequest);
@@ -76,13 +73,11 @@ public class StudentApi {
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete the student",
             description = "Delete student with ID")
-    @PreAuthorize("hasAuthority('ADMIN')")
     public StudentResponse deleteStudent(@PathVariable("id") Long id) {
         return studentService.deleteStudent(id);
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasAuthority('ADMIN')")
     @Operation(summary = "Gets a single student by identifier",
             description = "For valid response try integer IDs with value >= 1 and...")
     public StudentResponse findById(@PathVariable Long id) {
@@ -90,7 +85,6 @@ public class StudentApi {
     }
 
     @GetMapping("/getByName")
-    @PreAuthorize("hasAuthority('ADMIN')")
     @Operation(summary = "Get by name",
             description = "Get student by name")
     public Deque<StudentResponse> getByStudentName(@RequestParam String name) {
@@ -100,7 +94,6 @@ public class StudentApi {
     @Operation(summary = "Import EXCEL",
             description = "This endpoint for import students list from excel to group")
     @PostMapping("/import")
-    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public List<StudentResponse> importExcelFile(@RequestParam(name = "file") MultipartFile files, @RequestParam Long groupId) throws Exception {
         return studentService.importExcelFile(files, groupId);
     }
