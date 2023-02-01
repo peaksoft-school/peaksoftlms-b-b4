44b74473afdf781778694c781f75c443f5517bca
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-01 22:01:46 +0600

    refactor StudentApi v2

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/StudentApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/StudentApi.java
@@ -38,9 +38,7 @@ public class StudentApi {
     private final StudentService studentService;
     private final ResultService resultService;
 
-    @GetMapping
-    @Operation(summary = "Gets a list",
-            description = "Returns all students that are,if there are no students,then an error")
+    @Operation(summary = "Gets a list", description = "Returns all students that are,if there are no students,then an error")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
                     description = "Found the students",
@@ -48,67 +46,64 @@ public class StudentApi {
                             @Content(mediaType = "application/json",
                                     array = @ArraySchema(schema = @Schema(implementation = StudentApi.class)))})})
     @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
-    public StudentPaginationResponse getAll(@RequestParam int page,
-                                            @RequestParam int size,
-                                            @RequestParam StudyFormat studyFormat) {
-
+    @GetMapping
+    public StudentPaginationResponse getAll(@RequestParam int page, @RequestParam int size, @RequestParam StudyFormat studyFormat) {
         return studentService.getAll(--page, size, studyFormat);
     }
 
+
+    @Operation(summary = "Add new student", description = "This endpoint save new student. Only users with role admin can add new students")
     @PostMapping
-    @Operation(summary = "Add new student",
-            description = "This endpoint save new student. Only users with role admin can add new students")
     public StudentResponse saveStudent(@RequestBody @Valid StudentRequest studentRequest) {
         return studentService.saveStudent(studentRequest);
     }
 
-    @PutMapping("/{id}")
-    @Operation(summary = "Update the students",
-            description = "Updates the details of an endpoint with ID.  Only users with role admin can update students")
-    public StudentResponse updateStudent(@PathVariable("id") Long id,
-                                         @RequestBody @Valid StudentRequest studentRequest) {
+
+    @Operation(summary = "Update the students", description = "Updates the details of an endpoint with ID.  Only users with role admin can update students")
+    @PutMapping("{id}")
+    public StudentResponse updateStudent(@PathVariable("id") Long id, @RequestBody @Valid StudentRequest studentRequest) {
         return studentService.updateStudent(id, studentRequest);
     }
 
-    @DeleteMapping("/{id}")
-    @Operation(summary = "Delete the student",
-            description = "Delete student with ID")
+
+    @Operation(summary = "Delete the student", description = "Delete student with ID")
+    @DeleteMapping("{id}")
     public StudentResponse deleteStudent(@PathVariable("id") Long id) {
         return studentService.deleteStudent(id);
     }
 
-    @GetMapping("/{id}")
-    @Operation(summary = "Gets a single student by identifier",
-            description = "For valid response try integer IDs with value >= 1 and...")
+
+    @Operation(summary = "Gets a single student by identifier", description = "For valid response try integer IDs with value >= 1 and...")
+    @GetMapping("{id}")
     public StudentResponse findById(@PathVariable Long id) {
         return studentService.findById(id);
     }
 
-    @GetMapping("/getByName")
-    @Operation(summary = "Get by name",
-            description = "Get student by name")
+
+    @Operation(summary = "Get by name", description = "Get student by name")
+    @GetMapping("get-by-name")
     public Deque<StudentResponse> getByStudentName(@RequestParam String name) {
         return studentService.findByStudentName(name);
     }
 
-    @Operation(summary = "Import EXCEL",
-            description = "This endpoint for import students list from excel to group")
-    @PostMapping("/import")
+    @Operation(summary = "Import EXCEL", description = "This endpoint for import students list from excel to group")
+    @PostMapping("import")
     public List<StudentResponse> importExcelFile(@RequestParam(name = "file") MultipartFile files, @RequestParam Long groupId) throws Exception {
         return studentService.importExcelFile(files, groupId);
     }
 
-    @GetMapping("/studentCourses")
+
     @PreAuthorize("hasAnyAuthority('STUDENT')")
+    @GetMapping("student-courses")
     public Deque<CourseResponse> studentCourses(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         return studentService.getStudentCourses(user.getEmail());
     }
 
-    @PostMapping("test")
-    @Operation(summary = "Pass the test",
-            description = "This endpoint for pass the test")
+
+    @Operation(summary = "Pass the test", description = "This endpoint for pass the test")
     @PreAuthorize("hasAnyAuthority('STUDENT')")
+    @PostMapping("test")
     public AnswerResponse saveResult(Authentication authentication, @RequestBody AnswerRequest answerRequest) {
         User user = (User) authentication.getPrincipal();
         return resultService.saveResult(answerRequest, user.getEmail());
