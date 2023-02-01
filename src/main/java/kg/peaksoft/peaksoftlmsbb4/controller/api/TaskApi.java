922af57e24aa8ab2c013a475d1d1f2cb7d85cc87
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-01 22:05:19 +0600

    refactor TaskApi

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/TaskApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/TaskApi.java
@@ -5,15 +5,16 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.TaskRequest;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.TaskResponse;
 import kg.peaksoft.peaksoftlmsbb4.db.service.TaskService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("api/tasks")
-@Tag(name = "Task", description = "The Tasks API")
+@PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
 @CrossOrigin(origins = "*", maxAge = 3600)
+@Tag(name = "Task API", description = "The Tasks endpoints")
 public class TaskApi {
 
     private final TaskService taskService;
@@ -21,7 +22,6 @@ public class TaskApi {
     @PostMapping
     @Operation(summary = "Add new tasks",
             description = "This endpoint save new task. Only users with role teacher can add new task to lesson")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
     public TaskResponse saveTasks(@RequestBody TaskRequest taskRequest) {
         return taskService.saveTasks(taskRequest);
 
@@ -30,7 +30,7 @@ public class TaskApi {
     @GetMapping("/{id}")
     @Operation(summary = "Gets a single task by lesson identifier",
             description = "For valid response try integer IDs with value >= 1 and...")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTO','STUDENT')")
+    @PreAuthorize("hasAnyAuthority('STUDENT')")
     public TaskResponse findTaskByLessonId(@PathVariable Long id) {
         return taskService.findTaskByLessonId(id);
     }
@@ -38,13 +38,11 @@ public class TaskApi {
     @PutMapping("/{id}")
     @Operation(summary = "Update the tasks",
             description = "Updates the details of an endpoint with ID")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
     public TaskResponse update(@PathVariable Long id, @RequestBody TaskRequest taskRequest) {
         return taskService.update(id, taskRequest);
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
     @Operation(summary = "Delete the task",
             description = "Delete task with ID")
     public TaskResponse delete(@PathVariable Long id) {
@@ -54,7 +52,7 @@ public class TaskApi {
     @GetMapping("task/{id}")
     @Operation(summary = "Gets a single task by lesson identifier",
             description = "For valid response try integer IDs with value >= 1 and...")
-    @PreAuthorize("hasAnyAuthority('INSTRUCTOR','STUDENT')")
+    @PreAuthorize("hasAnyAuthority('STUDENT')")
     public TaskResponse getById(@PathVariable Long id) {
         return taskService.getById(id);
     }
