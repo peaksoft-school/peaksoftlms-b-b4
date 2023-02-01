045fb5cacf71733257a234c3b611a31045bc2814
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-01 21:51:23 +0600

    refactor QuestionApi v2

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/QuestionApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/QuestionApi.java
@@ -16,30 +16,28 @@ import javax.validation.Valid;
 @RequestMapping("api/questions")
 @PreAuthorize("hasAuthority('INSTRUCTOR')")
 @CrossOrigin(origins = "*", maxAge = 3600)
-@Tag(name = "Question", description = "The Question API")
+@Tag(name = "Question API", description = "The Question endpoints")
 public class QuestionApi {
+
     private final QuestionService questionService;
 
-    @Operation(summary = "Gets a single question by identifier",
-            description = "For valid response try integer IDs with value >= 1 and...")
-    @GetMapping("/{id}")
+    @Operation(summary = "Gets a single question by identifier", description = "For valid response try integer IDs with value >= 1 and...")
+    @GetMapping("{id}")
     public QuestionResponse findById(@Valid @PathVariable Long id) {
         return questionService.findById(id);
     }
 
-    @Operation(summary = "Update the question",
-            description = "Updates the details of an endpoint with ID")
-    @PutMapping("/{id}")
-    public QuestionResponse updateQuestion(
-            @Valid @PathVariable Long id, @RequestBody QuestionRequest questionRequest) {
-        return questionService.update(id, questionRequest);
+    @Operation(summary = "Update the question", description = "Updates the details of an endpoint with ID")
+    @PutMapping("{id}")
+    public QuestionResponse updateQuestion(@Valid @PathVariable Long id, @RequestBody QuestionRequest request) {
+        return questionService.update(id, request);
     }
 
-    @Operation(summary = "Delete question",
-            description = "Delete the question with ID")
-    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete question", description = "Delete the question with ID")
+    @DeleteMapping("{id}")
     public String deleteById(@Valid @PathVariable Long id) {
         questionService.delete(id);
         return String.format("successfully delete this id=%s", id);
     }
+
 }
