be185c29a86a3c2930e0a8963b399c4bcd7618ac
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-01 22:13:15 +0600

    refactor TeacherApi v2

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/TeacherApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/controller/api/TeacherApi.java
@@ -41,85 +41,78 @@ public class TeacherApi {
     private final GroupService groupService;
     private final CourseService courseService;
 
-    @GetMapping()
-    @Operation(summary = "Gets a list",
-            description = "Returns all teachers that are,if there are no teachers,then an error")
+
+    @Operation(summary = "Gets a list", description = "Returns all teachers that are,if there are no teachers,then an error")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
                     description = "Found the teachers",
                     content = {
                             @Content(mediaType = "application/json",
                                     array = @ArraySchema(schema = @Schema(implementation = TeacherApi.class)))})})
-    public TeacherPaginationResponse findAll(@RequestParam int page,
-                                             @RequestParam int size) {
+    @GetMapping()
+    public TeacherPaginationResponse findAll(@RequestParam int page, @RequestParam int size) {
         return teacherService.findAllTeacher(--page, size);
     }
 
-    @Operation(summary = "Add new teacher",
-            description = "This endpoint save new teacher")
+    @Operation(summary = "Add new teacher", description = "This endpoint save new teacher")
     @PostMapping
     public TeacherResponse saveTeacher(@Valid @RequestBody TeacherRequest teacherRequest) {
         return teacherService.saveTeacher(teacherRequest);
     }
 
-    @PatchMapping("/{id}")
-    @Operation(summary = "Update the teacher",
-            description = "Updates the details of an endpoint with ID")
+
+    @Operation(summary = "Update the teacher", description = "Updates the details of an endpoint with ID")
+    @PatchMapping("{id}")
     public TeacherResponse updateTeacher(@PathVariable("id") Long id, @RequestBody TeacherRequest teacherRequest) {
         return teacherService.updateTeacher(id, teacherRequest);
     }
 
-    @DeleteMapping("/{id}")
-    @Operation(summary = "Delete the teacher",
-            description = "Delete the teacher with ID")
+
+    @Operation(summary = "Delete the teacher", description = "Delete the teacher with ID")
+    @DeleteMapping("{id}")
     public TeacherResponse deleteTeacher(@PathVariable("id") Long id) {
         return teacherService.deleteTeacher(id);
     }
 
-    @Operation(summary = "Teacher's courses",
-            description = "This endpoint for get all teacher's courses")
-    @GetMapping("/teacherCourses")
+    @Operation(summary = "Teacher's courses", description = "This endpoint for get all teacher's courses")
     @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
+    @GetMapping("teacher-courses")
     public Deque<CourseResponse> teacherCourses(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         return teacherService.teacherCourses(user.getEmail());
     }
 
 
-    @Operation(summary = "Gets a single teacher by identifier",
-            description = "For valid response try integer IDs with value >= 1 and...")
-    @GetMapping("/{id}")
+    @Operation(summary = "Gets a single teacher by identifier", description = "For valid response try integer IDs with value >= 1 and...")
+    @GetMapping("{id}")
     public TeacherResponse findById(@PathVariable Long id) {
         return teacherService.findById(id);
     }
 
-    @Operation(summary = "Assign student to course",
-            description = "This endpoint for adding a student to a course. Only user with role teacher can add student to course")
-    @PostMapping("/assignStudent")
+    @Operation(summary = "Assign student to course", description = "This endpoint for adding a student to a course. Only user with role teacher can add student to course")
     @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
+    @PostMapping("assign-student")
     public StudentResponse assignStudentToCourse(@RequestBody AssignStudentRequest assignStudentRequest) {
         return studentService.assignStudentToCourse(assignStudentRequest);
     }
 
-    @Operation(summary = "Assign group to course",
-            description = "This endpoint for adding a group to course")
-    @PostMapping("/assignGroup")
+    @Operation(summary = "Assign group to course", description = "This endpoint for adding a group to course")
     @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
+    @PostMapping("assign-group")
     public String assignGroupToCourse(@RequestBody AssignGroupRequest assignGroupRequest) {
         return groupService.assignGroupToCourse(assignGroupRequest);
     }
 
-    @Operation(summary = "Course's teachers",
-            description = "Get all teachers ")
-    @GetMapping("teachersByCourseId/{id}")
+    @Operation(summary = "Course's teachers", description = "Get all teachers ")
+    @GetMapping("teachers-by-course/{id}")
     public List<TeacherResponse> getAllTeacherByCourseId(@PathVariable Long id) {
         return courseService.getAllTeacherByCourseId(id);
     }
 
-    @Operation(summary = "find all teachers",
-            description = "This method is a method of adding teachers to a course so you can give how many teachers per course ")
-    @GetMapping("Unassigned/{id}")
+    @Operation(summary = "find all teachers", description = "This method is a method of adding teachers to a course so you can give how many teachers per course ")
+    @GetMapping("unassigned/{id}")
     public Deque<TeacherResponse> teacherResponses(@PathVariable Long id) {
         return teacherService.teacherResponsesForAssign(id);
     }
+
 }
