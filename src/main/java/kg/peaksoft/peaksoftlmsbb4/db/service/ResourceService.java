ede0ec718435ad206e4b66f0c96513c206242df6
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-08 22:59:13 +0600

    refactor ResourceService

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/ResourceService.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/ResourceService.java
@@ -3,5 +3,7 @@ package kg.peaksoft.peaksoftlmsbb4.db.service;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.ResourceResponse;
 
 public interface ResourceService {
+
     ResourceResponse delete(Long id);
+
 }
