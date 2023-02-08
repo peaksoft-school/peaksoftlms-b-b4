436feebb9f3a33e1eba01cca7b48207f421f9b10
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-08 22:59:45 +0600

    refactor ResultService

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/ResultService.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/ResultService.java
@@ -5,7 +5,9 @@ import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.AnswerResponse;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.TestResultResponse;
 
 public interface ResultService {
+
     AnswerResponse saveResult(AnswerRequest answerRequest, String email);
 
     TestResultResponse getResults(Long id);
+
 }
