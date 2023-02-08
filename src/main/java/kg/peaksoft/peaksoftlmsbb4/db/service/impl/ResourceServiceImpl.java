2351f42f48935b9280feab844aef695f4a3c78ad
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-08 23:23:55 +0600

    refactor ResourceServiceImpl

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/impl/ResourceServiceImpl.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/impl/ResourceServiceImpl.java
@@ -9,20 +9,22 @@ import kg.peaksoft.peaksoftlmsbb4.exceptions.NotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
 @AllArgsConstructor
+@Service
 public class ResourceServiceImpl implements ResourceService {
+
     private final ResourceRepository resourceRepository;
     private final ResourceMapper resourceMapper;
 
     @Override
     public ResourceResponse delete(Long id) {
         if (resourceRepository.existsById(id)) {
-            Resource resource = resourceRepository.findById(id).orElseThrow(() -> new NotFoundException(String.format("Lesson with id %s not found", id)));
+            Resource resource = resourceRepository.findById(id).orElseThrow(() ->
+                    new NotFoundException(String.format("Lesson with id %s not found", id)));
             resourceRepository.deleteById(id);
-
             return resourceMapper.deConvert(resource);
         }
         return null;
     }
+
 }
