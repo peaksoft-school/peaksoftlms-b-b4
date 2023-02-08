0be445548b4c072bdba4bc41a3459a4895a25ca2
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-08 23:20:32 +0600

    refactor PresentationServiceImpl

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/impl/PresentationServiceImpl.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/impl/PresentationServiceImpl.java
@@ -16,23 +16,23 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
-@Service
-@AllArgsConstructor
 @Slf4j
 @Transactional
+@AllArgsConstructor
+@Service
 public class PresentationServiceImpl implements PresentationService {
+
     private final PresentationRepository presentationRepository;
     private final PresentationMapper presentationMapper;
     private final LessonRepository lessonRepository;
     private final AWSS3Service awss3Service;
 
     @Override
-    public PresentationResponse savePresentation(PresentationRequest presentationRequest) {
-        Lesson lessons = lessonRepository.findById(presentationRequest.getLessonId()).orElseThrow(() -> new NotFoundException(
-                String.format("Lesson with id %s not found", presentationRequest.getLessonId())
-        ));
+    public PresentationResponse savePresentation(PresentationRequest request) {
+        Lesson lessons = lessonRepository.findById(request.getLessonId()).orElseThrow(() ->
+                new NotFoundException(String.format("Lesson with id %s not found", request.getLessonId())));
         if (lessons.getPresentation() == null) {
-            Presentation presentation = presentationMapper.convert(presentationRequest);
+            Presentation presentation = presentationMapper.convert(request);
             Presentation save = presentationRepository.save(presentation);
             lessons.setPresentation(save);
             log.info("successfully save presentation:{}", presentation);
@@ -45,22 +45,23 @@ public class PresentationServiceImpl implements PresentationService {
     @Override
     public PresentationResponse findById(Long id) {
         log.info("successfully find by id:{}", id);
-        Presentation presentation = presentationRepository.findById(id).orElseThrow(() -> new NotFoundException(String.format("Not found id=%s", id)));
+        Presentation presentation = presentationRepository.findById(id).orElseThrow(() ->
+                new NotFoundException(String.format("Not found id=%s", id)));
         return presentationMapper.deConvert(presentation);
     }
 
     @Override
-    public PresentationResponse update(Long id, PresentationRequest presentationRequest) {
+    public PresentationResponse update(Long id, PresentationRequest request) {
         boolean exist = presentationRepository.existsById(id);
         if (!exist) {
             throw new NotFoundException(String.format("Not found id=%s", id));
         }
         Presentation presentation = getById(id);
-        if (!presentation.getName().equals(presentationRequest.getName())) {
-            presentation.setName(presentationRequest.getName());
+        if (!presentation.getName().equals(request.getName())) {
+            presentation.setName(request.getName());
         }
-        if (!presentation.getDescription().equals(presentationRequest.getDescription())) {
-            presentation.setDescription(presentationRequest.getDescription());
+        if (!presentation.getDescription().equals(request.getDescription())) {
+            presentation.setDescription(request.getDescription());
         }
         log.info("successfully update id:{}", id);
         return presentationMapper.deConvert(presentation);
@@ -75,7 +76,8 @@ public class PresentationServiceImpl implements PresentationService {
         }
         log.info("successfully delete presentation by id:{}", id);
         awss3Service.deleteFile(presentationRepository.getById(id).getFile());
-        Presentation presentation = presentationRepository.findById(id).orElseThrow(() -> new NotFoundException(String.format("Presentation with id %s not found", id)));
+        Presentation presentation = presentationRepository.findById(id).orElseThrow(() ->
+                new NotFoundException(String.format("Presentation with id %s not found", id)));
         presentationRepository.deleteById(id);
         return presentationMapper.deConvert(presentation);
     }
@@ -83,13 +85,14 @@ public class PresentationServiceImpl implements PresentationService {
     @Override
     public PresentationResponse findPresentationByLessonId(Long id) {
         Lesson lesson = lessonRepository.findById(id).orElseThrow(() ->
-                new NotFoundException(
-                        String.format("Lesson with id = %s not found", id)));
+                new NotFoundException(String.format("Lesson with id = %s not found", id)));
         return presentationMapper.deConvert(lesson.getPresentation());
     }
 
     private Presentation getById(Long id) {
         log.info("successfully get presentation by id:{}", id);
-        return presentationRepository.findById(id).orElseThrow(() -> new NotFoundException(String.format("Not found id=%s", id)));
+        return presentationRepository.findById(id).orElseThrow(() ->
+                new NotFoundException(String.format("Not found id=%s", id)));
     }
+
 }
