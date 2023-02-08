79e8c0f7e80eb2fb71b50d3c056fb38c321c82bf
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-08 22:47:56 +0600

    refactor AuthService

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/AuthService.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/AuthService.java
@@ -4,5 +4,7 @@ import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.AuthRequest;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.AuthResponse;
 
 public interface AuthService {
+
     AuthResponse authenticate(AuthRequest authRequest);
+
 }
