13c973ccbe61f016710c9e10dc08305e62a0584a
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-08 23:04:27 +0600

    refactor AuthServiceImpl

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/impl/AuthServiceImpl.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/impl/AuthServiceImpl.java
@@ -1,12 +1,12 @@
 package kg.peaksoft.peaksoftlmsbb4.db.service.impl;
 
+import kg.peaksoft.peaksoftlmsbb4.config.jwt.JwtUtils;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.AuthRequest;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.AuthResponse;
 import kg.peaksoft.peaksoftlmsbb4.db.model.User;
 import kg.peaksoft.peaksoftlmsbb4.db.repository.UserRepository;
 import kg.peaksoft.peaksoftlmsbb4.db.service.AuthService;
 import kg.peaksoft.peaksoftlmsbb4.exceptions.NotFoundException;
-import kg.peaksoft.peaksoftlmsbb4.config.jwt.JwtUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,27 +14,24 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-@Service
 @Slf4j
+@Service
 @AllArgsConstructor
 public class AuthServiceImpl implements AuthService {
+
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
     private final UserRepository userRepository;
-
 
     @Override
     public AuthResponse authenticate(AuthRequest authRequest) {
         Authentication authentication;
 
-        authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                authRequest.getEmail(),
-                authRequest.getPassword()
-        ));
+        authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
 
-        User user = userRepository.findByEmail(authRequest.getEmail()).orElseThrow(() -> new NotFoundException(
-                "user with this email does not exists"
-        ));
+        User user = userRepository.findByEmail(authRequest.getEmail()).orElseThrow(() ->
+                new NotFoundException("user with this email does not exists"));
         String generatedToken = jwtUtils.generateToken(authentication);
         log.info("successful generated token :{}", generatedToken);
         return AuthResponse.builder()
@@ -43,4 +40,5 @@ public class AuthServiceImpl implements AuthService {
                 .role(user.getRole())
                 .build();
     }
+
 }
