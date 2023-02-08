70c52d8037b8c004f97b7e1911803ccb43b0c4d9
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-08 23:51:15 +0600

    refactor BadRequestException

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/exceptions/BadRequestException.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/exceptions/BadRequestException.java
@@ -1,13 +1,13 @@
 package kg.peaksoft.peaksoftlmsbb4.exceptions;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@Slf4j
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
+
     public BadRequestException(String message) {
         super(message);
     }
+
 }
