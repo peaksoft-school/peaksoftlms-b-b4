e0cc4e19539d017c7a4735b1876e915f5235a8f3
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-08 22:48:29 +0600

    refactor CourseService

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/CourseService.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/db/service/CourseService.java
@@ -1,10 +1,10 @@
 package kg.peaksoft.peaksoftlmsbb4.db.service;
 
-import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.CoursePaginationResponse;
+import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.AssignTeacherRequest;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.CourseRequest;
+import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.CoursePaginationResponse;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.CourseResponse;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.StudentResponse;
-import kg.peaksoft.peaksoftlmsbb4.controller.payload.request.AssignTeacherRequest;
 import kg.peaksoft.peaksoftlmsbb4.controller.payload.response.TeacherResponse;
 
 import java.util.List;
@@ -28,4 +28,5 @@ public interface CourseService {
     List<TeacherResponse> getAllTeacherByCourseId(Long id);
 
     String assignTeachersToCourse(AssignTeacherRequest assignTeacherRequest);
+
 }
