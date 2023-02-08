f87d80153eaec8dcb5973716ce628600b3807998
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-02-08 22:45:44 +0600

    refactor VariantRepository

--- src/main/java/kg/peaksoft/peaksoftlmsbb4/db/repository/VariantRepository.java
+++ src/main/java/kg/peaksoft/peaksoftlmsbb4/db/repository/VariantRepository.java
@@ -4,8 +4,6 @@ import kg.peaksoft.peaksoftlmsbb4.db.model.Variant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface VariantRepository extends JpaRepository<Variant, Long> {
 
